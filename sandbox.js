11d0517e1cce9d70e58df1d857ddb7eaf195563a
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-04 10:49:14 +0300

    Type appending

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,25 @@
-var character = 'Terminator';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
-// tsc sandbox.ts
-// compiles the code. Needs to be exact name with .js file
-// tsc sandbox.ts -w
-// auto-compiles
+// const character = 'Terminator';
+// console.log(character);
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+// // tsc sandbox.ts
+// // compiles the code. Needs to be exact name with .js file
+// // tsc sandbox.ts -w
+// // auto-compiles
+// // ------------------------------------------------------
+var char = 'Predator';
+var age = 324;
+var isKiller = true;
+// age = 55
+// age = false
+// problems arise when types changes
+// const circ = (diameter) => diameter * Math.PI;
+// console.log(circ("66"));
+// // gets NaN
+var circ = function (diameter) { return diameter * Math.PI; };
+// :type fixes issue
+console.log(circ(55));
+// // gets NaN
--- sandbox.ts
+++ sandbox.ts
@@ -1,16 +1,35 @@
-const character = 'Terminator';
+// const character = 'Terminator';
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll('input');
+// const inputs = document.querySelectorAll('input');
 
-console.log(inputs);
+// console.log(inputs);
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
 
-// tsc sandbox.ts
-// compiles the code. Needs to be exact name with .js file
-// tsc sandbox.ts -w
-// auto-compiles
+// // tsc sandbox.ts
+// // compiles the code. Needs to be exact name with .js file
+// // tsc sandbox.ts -w
+// // auto-compiles
+// // ------------------------------------------------------
+
+let char = 'Predator';
+let age = 324;
+let isKiller = true;
+
+// age = 55
+// age = false
+// problems arise when types changes
+
+// const circ = (diameter) => diameter * Math.PI;
+
+// console.log(circ("66"));
+// // gets NaN
+
+const circ = (diameter: number) => diameter * Math.PI;
+// :type fixes issue
+console.log(circ(55));
+// // gets NaN
