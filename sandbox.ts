d086b851eefc2f84601608279f2c174285f25d51
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-04 10:41:19 +0300

    Basics running TS in js

--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,15 @@
-var character = 'Terminator';
+const character = 'Terminator';
+
 console.log(character);
-var inputs = document.querySelectorAll('input');
+
+const inputs = document.querySelectorAll('input');
+
 console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
+
+inputs.forEach((input) => {
+  console.log(input);
 });
+
 // tsc sandbox.ts
 // compiles the code. Needs to be exact name with .js file
 // tsc sandbox.ts -w
