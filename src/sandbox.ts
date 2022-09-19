a5dcaaa474245136f50da169f6053b0a7eec6e18
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-19 17:01:40 +0300

    Starting new section of the tutorial

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -163,28 +163,28 @@
 
 // let greet: Function
 
-//example 1
-let greet: (a: string, b: string) => void;
+// example 1
+// let greet: (a: string, b: string) => void;
 
-greet = (name: string, greeting: string) => {
-  console.log(`${name} says ${greeting} `);
-};
-//example 2
-let calc: (a: number, b: number, C: string) => number;
-calc = (numOne: number, numTwo: number, action: string) => {
-  if (action === 'add') {
-    return numOne + numTwo;
-  } else {
-    return numOne - numTwo;
-  }
-};
+// greet = (name: string, greeting: string) => {
+//   console.log(`${name} says ${greeting} `);
+// };
+// //example 2
+// let calc: (a: number, b: number, C: string) => number;
+// calc = (numOne: number, numTwo: number, action: string) => {
+//   if (action === 'add') {
+//     return numOne + numTwo;
+//   } else {
+//     return numOne - numTwo;
+//   }
+// };
 
-//example 3
+// //example 3
 
-let logDetails: (obj: { name: string; age: number }) => void;
+// let logDetails: (obj: { name: string; age: number }) => void;
 
-type person = { name: string; age: number };
+// type person = { name: string; age: number };
 
-logDetails = (ninja: person) => {
-  console.log(`${ninja.name} is ${ninja.age} years old`);
-};
+// logDetails = (ninja: person) => {
+//   console.log(`${ninja.name} is ${ninja.age} years old`);
+// };
